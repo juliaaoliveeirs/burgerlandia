d382a038c1b26df7472c8001b4af149f24ddb17d
Julia Silva <julia.oliveira.silva@outlook.com>
2023-07-04 21:47:09 -0300

    finalização da página index com animação js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function initAnimationScroll() {
+  const elements = document.querySelectorAll('.js-scroll');
+
+  if (elements.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animationScroll() {
+      elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const isElementVisible = elementTop - windowHalf < 0;
+
+        if (isElementVisible) element.classList.add('ativo');
+      });
+    }
+
+    animationScroll();
+
+    window.addEventListener('scroll', animationScroll);
+  }
+}
+initAnimationScroll();
